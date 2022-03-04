9b735d1c6d51d4fbb4cac76dc54fa27e1f2552b4
Deigan Millikin <deiganmillikin@gmail.com>
2022-03-04 11:10:03 -0700

    feat: create hide/show button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
-import Philosophers from './components/Philosophers';
+import { useState } from 'react'; 
+// probably not needed
+// import Philosophers from './components/Philosophers';
 
 function App() {
+  const [ showNames, setShowNames] = useState(true)
   const philosophers = ["Marcus Aurelius", "Epictetus", "Seneca", "Zeno"]
 
   return (
@@ -11,14 +14,20 @@ function App() {
       <Philosophers name="Epictetus" />
       <Philosophers name="Seneca" />
       <Philosophers name="Zeno" /> */}
-      <div>
-        {philosophers.map((name, index) => {
+      <div className="philosopher-container">
+        {showNames && philosophers.map((name, index) => {
           return (
             <div key={index} className="philosopher-container">
               <h2>{name}</h2>
             </div>
           )
         })}
+        {showNames && <div>
+          <button onClick={() => setShowNames(false)}>Hide philosophers</button>
+        </div>}
+        {!showNames && <div>
+          <button onClick={() => setShowNames(true)}>Show philosophers</button>
+        </div>}
       </div>
     </div>
   );
--- src/components/Philosophers.js
+++ src/components/Philosophers.js
@@ -2,7 +2,7 @@ import './Philosophers.css';
 
 export default function Philosophers({ name }) {
 	return (
-		<div className="philosopher-container">
+		<div>
 			<h2>{name}</h2>
 		</div>
 	)
