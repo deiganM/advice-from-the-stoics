8879a5feb0162010b10de2ad0032f632f900eb1f
Deigan Millikin <deiganmillikin@gmail.com>
2022-03-18 21:48:39 -0600

    added some style

--- src/App.js
+++ src/App.js
@@ -5,27 +5,29 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Advice From the Stoics</h1>
-      <Philosophers 
-        name={"Marcus Aurelius"}
-        image={'../images/MarcusAurelius.jpg'}
-        alt={"Marcus Aurelius"}
-      />
-      <Philosophers 
-        name={"Epictetus"}
-        image={'../images/epictetus.jpg'}
-        alt={"Epictetus"}
-      />
-      <Philosophers 
-        name={"Seneca the Younger"}
-        image={'../images/SenecaTheYounger.jpg'}
-        alt={"Seneca the Younger"}
-      />
-      <Philosophers 
-        name={"Zeno of Citium"}
-        image={'../images/ZenoOfCitium.jpg'}
-        alt={"Zeno of Citium"}
-      />
+      <h1 className='title'>Advice From the Stoics</h1>
+      <div className="container">
+        <Philosophers 
+          name={"Marcus Aurelius"}
+          image={'../images/MarcusAurelius.jpg'}
+          alt={"Marcus Aurelius"}
+        />
+        <Philosophers 
+          name={"Epictetus"}
+          image={'../images/epictetus.jpg'}
+          alt={"Epictetus"}
+        />
+        <Philosophers 
+          name={"Seneca the Younger"}
+          image={'../images/SenecaTheYounger.jpg'}
+          alt={"Seneca the Younger"}
+        />
+        <Philosophers 
+          name={"Zeno of Citium"}
+          image={'../images/ZenoOfCitium.jpg'}
+          alt={"Zeno of Citium"}
+        />
+      </div>
     </div>
   );
 }
